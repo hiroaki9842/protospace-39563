312f40bd1c5ed840b0e558a964fc042a333d6e4a
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-07-07 19:43:45 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Protospace39563
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'protospace-39563.onrender.com'
   end
 end
