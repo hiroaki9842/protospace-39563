239d9e63c93af20c3071c8bdd33ba6054dae9aee
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-07-10 15:40:05 +0900

    ユーザーのバリデーション設定完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
   has_many :prototypes
   has_many :comments
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
 end
